5d609917b127c154e58d0973489eed8b42580810
Ryan Buchanan <buchanan.ryan22@gmail.com>
2020-09-13 19:01:07 -0600

    Imported sklearn tools

--- Multiple_Linear_Regression.py
+++ Multiple_Linear_Regression.py
@@ -6,3 +6,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Import the data set
+dataset = pd.read_csv("50_Startups.csv")
+X = dataset.iloc[: , :-1].values
+y = dataset.iloc[:, -1].values
+
+# Encode categories
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers = [('encoder', OneHotEncoder(), [0])], remainder = 'passthrough')
+X = np.array(ct.fit_transform(X))
+
+# Split dataset into training and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Train
+
+# Predict the Test set 
