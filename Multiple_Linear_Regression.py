07c22c068f599b52d98d25fc5a44ba4a2d395363
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-17 08:11:24 -0600

    Train the Multiple Linear Regression complete w/ prediciton for single potential case

--- Multiple_Linear_Regression.py
+++ Multiple_Linear_Regression.py
@@ -34,3 +34,24 @@ regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 # Predict the Test set results
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+# Making a single prediction (for example the profit of a startup with R&D Spend = 160000, Administration Spend = 130000, Marketing Spend = 300000 and State = 'California')
+print("Predicted startup profit = $ regressor.predict([[1,0,0,160000, 130000, 300000]]))
+
+# Therefore, our model predicts that the profit of a Californian startup which spent 160000 in R&D, 130000 in Administration and 300000 in Marketing is $ 181566,92.
+
+# Important note 1: Notice that the values of the features were all input in a double pair of square brackets. That's because the "predict" method always expects a 2D array as the format of its inputs. And putting our values into a double pair of square brackets makes the input exactly a 2D array. Simply put:
+
+# 1,0,0,160000,130000,300000→scalars 
+
+# [1,0,0,160000,130000,300000]→1D array 
+
+# [[1,0,0,160000,130000,300000]]→2D array 
+
+# Important note 2: Notice also that the "California" state was not input as a string in the last column but as "1, 0, 0" in the first three columns. That's because of course the predict method expects the one-hot-encoded values of the state, and as we see in the second row of the matrix of features X, "California" was encoded as "1, 0, 0". And be careful to include these values in the first three columns, not the last three ones, because the dummy variables are always created in the first columns.
+
+print(regressor.coef_)
+print("Y-intercept or start profit = $", round(regressor.intercept_, 2))
